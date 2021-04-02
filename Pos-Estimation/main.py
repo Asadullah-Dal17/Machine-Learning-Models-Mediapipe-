010402345cee3c7d051268ba6876cc3c83205c87
Asadullah-Dal17 <aiphile17@gmail.com>
2021-04-02 12:44:06 +0500

    adding main.py pos estimation code

--- Pos-Estimation/main.py
+++ Pos-Estimation/main.py
@@ -11,31 +11,41 @@ fonts = cv.FONT_HERSHEY_COMPLEX
 # objects
 camera = cv.VideoCapture(capID)
 
-while True:
-  # starting frame counter
-  frame_count += 1
+# Mdeda pipe objects 
+mpPose = mp.solutions.pose
 
-  # read frame from the camera
-  ret, frame = camera.read()
+with mpPose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as Pose:
+	
+	while True:
+	# starting frame counter
+		frame_count += 1
 
-  # calculating the taken by total frame
-  seconds = time.time() - startTime
+		# read frame from the camera
+		ret, frame = camera.read()
+		if not ret:
+			print("if camera is loaded or not")
+			continue 
+		# converting BGR to RGB frame 
+		RGB_Frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
-  # calculating the frames per seconds FPS in short
-  fps = frame_count / seconds
+		# calculating the taken by total frame
+		seconds = time.time() - startTime
 
-  # show the fps on the screen
-  cv.putText(frame, f"FPS : {int(fps)}", (50, 20), fonts, 0.7, (0, 255, 255), 1)
+		# calculating the frames per seconds FPS in short
+		fps = frame_count / seconds
 
-  # show the frame on the screen
-  cv.imshow("frame", frame)
+		# show the fps on the screen
+		cv.putText(frame, f"FPS : {int(fps)}", (50, 20), fonts, 0.7, (0, 255, 255), 1)
 
-  # define the key
-  key = cv.waitKey(1)
+		# show the frame on the screen
+		cv.imshow("frame", frame)
 
-  # break the loop if "q" is pressed on the keyboard
-  if key == ord('q'):
-    break
+		# define the key
+		key = cv.waitKey(1)
+
+		# break the loop if "q" is pressed on the keyboard
+		if key == ord('q'):
+			break
 
 # close all the windows which has been create
 cv.destroyAllWindows()
