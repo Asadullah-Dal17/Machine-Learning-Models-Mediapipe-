446b2d354ddf974f3fe195b5d91af0396fc6f406
Asadullah-Dal17 <aiphile17@gmail.com>
2021-04-10 14:28:52 +0500

    Updating Lib

--- Holistic-Pos/holistic_pose.py
+++ Holistic-Pos/holistic_pose.py
@@ -19,63 +19,6 @@ MAGENTA = (255, 0, 255)
 WHITE = (255, 255, 255)
 ORANGE = ( 255, 125, 0)
 # create function
-# Getting hand points (Lnadmarks ) 
-def getHandPoints(image, landmark, colour=(0,255,0),radius=2):
-    # option = [", "right_hand_landmarks"]
-        # print(mpOut.op)
-    linesPoints = []
-    for ID, marks in enumerate(landmark):
-        # print(ID, "  ", marks)
-        
-        height, width, _ = image.shape
-
-        pX, pY = int(width * marks.x), int(height * marks.y)
-        
-        # cv.circle(image, (pX, pY), radius, colour, -1)
-
-        # updating hand points to the list 
-        linesPoints.append((pX, pY))
-        
-        # cv.putText(image, f'{ID}', (pX, pY), fonts, 0.3, RED, 1)
-    # print(linesPoints)
-    # print(tuple(linesPoints[0:4]))
-    # cv.circle(image, tuple(linesPoints[0]), 6, (0, 255,255), 3)
-
-    # print(dicPoints )
-    # dviding the list into different hand parts
-
-    thumb = linesPoints[1:5]
-    indexFinger = linesPoints[5:9]
-    middleFinger = linesPoints[9:13]
-    ringFinger = linesPoints[13:17]
-    pinkyFinger = linesPoints[17:21]
-
-    # print(newPoints)
-    # cv.circle(image, tuple(newPoints[0]), 6, (0, 255, 255), 3)
-    # cv.circle(image, tuple(newPoints[1]), 6, (255, 255, 255), 3)
-    # cv.circle(image, tuple(newPoints[2]), 6, (0, 25,255), 3)
-    # pts = np.array([index], np.int32)
-    # pts = pts.reshape((-1, 1, 2))
-    # # print(pts)
-    # print()
-    # img = cv.polylines(image,[pts],False,(0,255,255),2)
-    return thumb, indexFinger, middleFinger, ringFinger, pinkyFinger
-# drawing the lines and circles on the hand 
-def handDraw(image,listOfTuples, color, thickness,DrawCircle=False ,radius=5,circleColor=(255,255,255), CType=2 ):
-        listOfList = list(map(list, listOfTuples))
-        if DrawCircle == True:
-            
-            for points in listOfTuples:
-                cv.circle(image, points, radius, circleColor, CType)
-            # print(points)
-        # print(listOfList)
-        pts = np.array([listOfList], np.int32)
-        # pts = np.array([index], np.int32)
-        pts = pts.reshape((-1, 1, 2))
-        # print(pts)
-        # print()
-        img = cv.polylines(image,[pts],False,color,3)
-
 # objects 
 camera = cv.VideoCapture(cameraID)
 
@@ -116,24 +59,24 @@ with mpHolistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0
         if results.left_hand_landmarks:
             # print("True")
             # getting the hand points individual part of hand
-            thumbLeft, indexLeft, middleLeft, ringLeft, pinkyLeft = getHandPoints(mask, results.left_hand_landmarks.landmark)
+            thumbLeft, indexLeft, middleLeft, ringLeft, pinkyLeft = Lib.getHandPoints(mask, results.left_hand_landmarks.landmark)
             # print(thumbLeft)
             # Calling hand point draw function 
-            handDraw(mask, thumbLeft, GREEN, 2, True)
-            handDraw(mask, indexLeft, RED, 2, True)
-            handDraw(mask, middleLeft, BLUE,2, True, 4, (255,0, 244))
-            handDraw(mask, ringLeft, YELLOW, 2,True, 4, (0,255,0) )
-            handDraw(mask, pinkyLeft, ORANGE, 2, True)
+            Lib.handDraw(mask, thumbLeft, GREEN, 2, True)
+            Lib.handDraw(mask, indexLeft, RED, 2, True)
+            Lib.handDraw(mask, middleLeft, BLUE,2, True, 4, (255,0, 244))
+            Lib.handDraw(mask, ringLeft, YELLOW, 2,True, 4, (0,255,0) )
+            Lib.handDraw(mask, pinkyLeft, ORANGE, 2, True)
             
 
         if results.right_hand_landmarks:
             # print("True")
-            thumbRight, indexRight, middleRight, ringRight, pinkyRight = getHandPoints(mask, results.right_hand_landmarks.landmark)
-            handDraw(mask, thumbRight, GREEN, 2, True)
-            handDraw(mask, indexRight, RED, 2, True)
-            handDraw(mask, middleRight, BLUE, 2, True)
-            handDraw(mask, ringRight, YELLOW, 2, True)
-            handDraw(mask, pinkyRight, ORANGE, 2, True)
+            thumbRight, indexRight, middleRight, ringRight, pinkyRight = Lib.getHandPoints(mask, results.right_hand_landmarks.landmark)
+            Lib.handDraw(mask, thumbRight, GREEN, 2, True)
+            Lib.handDraw(mask, indexRight, RED, 2, True)
+            Lib.handDraw(mask, middleRight, BLUE, 2, True)
+            Lib.handDraw(mask, ringRight, YELLOW, 2, True)
+            Lib.handDraw(mask, pinkyRight, ORANGE, 2, True)
         if results.pose_landmarks:
             Lib.PoseLandmarks(frame, results.pose_landmarks.landmark)
         #  finding frame per seconds 
