5f01826378268a61cd45021cc478217f5e69d3cf
Asadullah-Dal17 <aiphile17@gmail.com>
2021-04-10 12:09:55 +0500

    updating hand Draw funciton

--- Holistic-Pos/holistic_pose.py
+++ Holistic-Pos/holistic_pose.py
@@ -13,13 +13,15 @@ fonts = cv.FONT_HERSHEY_COMPLEX
 RED = (0, 0, 255)
 GREEN = (0, 255, 0)
 BLUE = (255, 0, 0)
-
+YELLOW = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+WHITE = (255, 255, 255)
+ORANGE = ( 255, 125, 0)
 # create function 
 def getHandPoints(image, landmark, colour=(0,255,0),radius=2):
     # option = [", "right_hand_landmarks"]
         # print(mpOut.op)
     linesPoints = []
-    # dicPoints = {}
     for ID, marks in enumerate(landmark):
         # print(ID, "  ", marks)
         
@@ -27,23 +29,25 @@ def getHandPoints(image, landmark, colour=(0,255,0),radius=2):
 
         pX, pY = int(width * marks.x), int(height * marks.y)
         
-        cv.circle(image, (pX, pY), radius, colour, -1)
+        # cv.circle(image, (pX, pY), radius, colour, -1)
 
         # updating hand points to the list 
         linesPoints.append((pX, pY))
-        # dicPoints.update({ID: [pX, pY]})
         
-        cv.putText(image, f'{ID}', (pX, pY), fonts, 0.3, RED, 1)
+        # cv.putText(image, f'{ID}', (pX, pY), fonts, 0.3, RED, 1)
     # print(linesPoints)
     # print(tuple(linesPoints[0:4]))
     # cv.circle(image, tuple(linesPoints[0]), 6, (0, 255,255), 3)
 
     # print(dicPoints )
+    # dviding the list into different hand parts
+
     thumb = linesPoints[1:5]
     indexFinger = linesPoints[5:9]
     middleFinger = linesPoints[9:13]
     ringFinger = linesPoints[13:17]
     pinkyFinger = linesPoints[17:21]
+
     # print(newPoints)
     # cv.circle(image, tuple(newPoints[0]), 6, (0, 255, 255), 3)
     # cv.circle(image, tuple(newPoints[1]), 6, (255, 255, 255), 3)
@@ -54,13 +58,18 @@ def getHandPoints(image, landmark, colour=(0,255,0),radius=2):
     # print()
     # img = cv.polylines(image,[pts],False,(0,255,255),2)
     return thumb, indexFinger, middleFinger, ringFinger, pinkyFinger
-def LinesFunc(image,listOfTuples, color, thickness):
+def LinesFunc(image,listOfTuples, color, thickness,DrawCircle=False ,radius=5,circleColor=(255,255,255), CType=2 ):
         listOfList = list(map(list, listOfTuples))
-        print(listOfList)
+        if DrawCircle == True:
+            
+            for points in listOfTuples:
+                cv.circle(image, points, radius, circleColor, CType)
+            # print(points)
+        # print(listOfList)
         pts = np.array([listOfList], np.int32)
         # pts = np.array([index], np.int32)
         pts = pts.reshape((-1, 1, 2))
-        print(pts)
+        # print(pts)
         # print()
         img = cv.polylines(image,[pts],False,color,3)
 # objects 
@@ -104,6 +113,11 @@ with mpHolistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0
             thumbLeft, indexLeft, middleLeft, ringLeft, pinkyLeft = getHandPoints(mask, results.left_hand_landmarks.landmark)
             # print(thumbLeft)
             LinesFunc(mask, thumbLeft, GREEN, 2)
+            LinesFunc(mask, indexLeft, RED, 2)
+            LinesFunc(mask, middleLeft, BLUE,2, True, 4, (255,0, 244))
+            LinesFunc(mask, ringLeft, YELLOW, 2,True, 4, (0,255,0) )
+            LinesFunc(mask, pinkyLeft, ORANGE, 2)
+            
 
         if results.right_hand_landmarks:
             # print("True")
