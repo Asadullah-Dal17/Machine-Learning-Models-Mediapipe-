97e82a9c131837792679c2bbb78e1426e0b5ca17
Asadullah-Dal17 <aiphile17@gmail.com>
2021-04-11 19:25:50 +0500

    updating hand pos Lib file

--- handPos/Lib.py
+++ handPos/Lib.py
@@ -4,6 +4,16 @@ import mediapipe as mp
 import math
 
 class handDetector():
+    # colours
+    YELLOW = (0, 255, 255)
+    CYAN = (255, 255, 0)
+    MAGENTA = (255, 0, 255)
+    GOLDEN = (32, 218, 165)
+    LIGHT_BLUE = (255, 30, 144)
+    PURPLE = (128, 0, 128)
+    CHOCOLATE = (30, 105, 210)
+    PINK = (147, 20, 255)
+    ORANGE = (0, 69, 255)
     def __init__(self, mode=False, max_num_hands=2, min_detection_conf=0.5, min_tracking_conf=0.5):
         self.mode = mode
         self.max_num_hands = max_num_hands
@@ -18,10 +28,14 @@ class handDetector():
         # image.flags.writeable = False
         # get results from hand pos estimator
         RGB_image =cv.cvtColor(image, cv.COLOR_BGR2RGB)
-        print("working_")
+        # print("working_")
         results = self.hands.process(RGB_image)
         RGB_image.flags.writeable = True
         if results.multi_hand_landmarks:
-            print("land marks are detected")
-        # return image
+            for handMarks in results.multi_hand_landmarks:
+                # print(handMarks)
+                for IDs, HandPoints in enumerate(handMarks.landmark):
+                    print(f"{IDs}  {HandPoints}")
+            # print("land marks are detected")
+        return image
 
